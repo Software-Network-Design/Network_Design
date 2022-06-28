87c1685dac905a51fa243cd38e8b41d81b451d39
MrVulcan <wkr1wkr@163.com>
2022-06-28 23:49:59 +0800

    实验文档 完全无用

---
+++ Client/try.py
@@ -0,0 +1,12 @@
+import tkinter
+root = tkinter.Tk()
+menu = tkinter.Menu(root,tearoff=0)
+menu.add_command(label="Copy")
+menu.add_command(label="Paste")
+menu.add_separator()
+menu.add_command(label="Cut")
+def popupmenu(event):
+    menu.post(event.x_root,event.y_root)
+root.bind("<Button-2>",popupmenu)
+root.mainloop()
+
