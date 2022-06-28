d8a176841f469f5c69d990e754389f31d242df92
zuobiao-zhou <1373529784@qq.com>
2022-06-28 10:59:11 +0800

    更改服务器思路为“一直侦听”

--- Server/Server_Mock.py
+++ Server/Server_Mock.py
@@ -33,104 +33,86 @@ class chat_server(threading.Thread):
     # addr ——— (host,port)
     # conn ——— socket
     def connect(self, conn, addr):
+        try:
+            while True:
+                data = conn.recv(1024)
+                data = json.loads(data.decode('uf-8'))
+                if not data:
+                    break
+                # 处理之后接收到的各类型消息
+                else:
+                    if request['type'] == 1:
+                        message = {
+                            'send': 'server',
+                            'receive': '',
+                            'type': 2,
+                            'info': {
+                                # sucess：无此用户、密码错误、登陆成功
+                                'success': '',
+                                'friends': {
+
+                                }
+                            }
+                        }
+                        user_id = request['info']['user_id']
+                        user_pwd = request['info']['user_pwd']
+                        message['receive'] = user_id
+                        sql = "select * from User where user_id = '%s'" %(user_id)
+                        # print(sql)
+                        try:
+                            cursor.execute(sql)
+                            user = cursor.fetchone()
+                            user_name = user[1]
+                            # 登陆成功
+                            if user[4] == user_pwd:
+                                self.online = 1
+                                message['info']['success'] = '登陆成功'
+                                sql2 = "select * from User_Friends where user1_id = '%s' or user2_id = '%s'" %(user_id, user_id)
+                                # print(sql2)
+                                try:
+                                    cursor.execute(sql2)
+                                    user_friends = cursor.fetchall()
+                                    i = 0
+                                    for friend in user_friends:
+                                        # print(friend)
+                                        if(user_id != friend[0]):
+                                            message['info']['friends']['friend'+str(i)]={
+                                                'user_id': friend[0]
+                                            }
+                                        else:
+                                            message['info']['friends']['friend'+str(i)]={
+                                                'user_id': friend[1]
+                                            }
+                                        i = i + 1
+                                    message['info']['friends']['friends_num'] = i
+                                except:
+                                    message['info']['friends']['friends_num'] = 0
+                            # 密码错误
+                            else:
+                                message['info']['success'] = '密码错误'
+                        # 无此用户
+                        except:
+                            message['info']['success'] = '无此用户'
+                        # 登陆成功
+                        if self.online == 1:
+                            users.append((conn, user_id, user_name, addr))
+                            message = json.dumps(message, ensure_ascii=False)
+                            conn.send(message.encode('utf-8'))
+                        else :
+                            message = json.dumps(message, ensure_ascii=False)
+                            conn.send(message.encode('utf-8'))
+        # 断开连接
+        except:
+            pass
+        finally:
+            conn.close()
+            self.online = 0
+            self.delUser(conn)
+            print(user_id+'断开连接！')
         request = conn.recv(RCV_SIZE)
         request = json.loads(request.decode('utf-8'))
-        # 注册请求
-        # 
-        # 登录请求
-        if request['type'] == 1:
-            message = {
-                'send': 'server',
-                'receive': '',
-                'type': 2,
-                'info': {
-                    # sucess：无此用户、密码错误、登陆成功
-                    'success': '',
-                    'friends': {
 
-                    },
-                    'groups': {
-
-                    }
-                }
-            }
-            user_id = request['info']['user_id']
-            user_pwd = request['info']['user_pwd']
-            message['receive'] = user_id
-            sql = "select * from User where user_id = '%s'" %(user_id)
-            # print(sql)
-            try:
-                cursor.execute(sql)
-                user = cursor.fetchone()
-                user_name = user[1]
-                # 登陆成功
-                if user[4] == user_pwd:
-                    self.online = 1
-                    message['info']['success'] = '登陆成功'
-                    sql2 = "select * from User_Friends where user1_id = '%s' or user2_id = '%s'" %(user_id, user_id)
-                    # print(sql2)
-                    try:
-                        cursor.execute(sql2)
-                        user_friends = cursor.fetchall()
-                        i = 0
-                        for friend in user_friends:
-                            # print(friend)
-                            if(user_id != friend[0]):
-                                message['info']['friends']['friend'+str(i)]={
-                                    'user_id': friend[0]
-                                }
-                            else:
-                                message['info']['friends']['friend'+str(i)]={
-                                    'user_id': friend[1]
-                                }
-                            i = i + 1
-                        message['info']['friends']['friends_num'] = i
-                    except:
-                        message['info']['friends']['friends_num'] = 0
-                    sql3 = "select group_id from User_Group where user_id = '%s'" %(user_id)
-                    # print(sql3)
-                    try:
-                        cursor.execute(sql3)
-                        groups_id = cursor.fetchall()
-                        i = 0
-                        for group_id in groups_id:
-                            message['info']['groups']['group'+str(i)]['group_id'] = group_id
-                            i = i + 1
-                        message['info']['groups']['groups_num'] = i
-                    except:
-                        message['info']['groups']['groups_num'] = 0
-                # 密码错误
-                else:
-                    message['info']['success'] = '密码错误'
-            # 无此用户
-            except:
-                message['info']['success'] = '无此用户'
-        # 登陆成功
-        if self.online == 1:
-            users.append((conn, user_id, user_name, addr))
-            message = json.dumps(message, ensure_ascii=False)
-            conn.send(message.encode('utf-8'))
-            # 登陆成功，开始一直侦听
-            try:
-                while True:
-                    data = conn.recv(1024)
-                    data = data.decode()
-                    if not data:
-                        break
-                    # 处理之后接收到的各类型消息
-            # 断开连接
-            except:
-                pass
-            finally:
-                conn.close()
-                self.online = 0
-                self.delUser(conn)
-                print(user_id+'断开连接！')
-
-        # 登陆失败
-        else :
-            message = json.dumps(message, ensure_ascii=False)
-            conn.send(message.encode('utf-8')) 
+         
 
     # addr ——— (host,port)
     # 将聊天消息保存到队列
