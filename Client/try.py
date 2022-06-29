624a3e539ab3e7f04b4d63741fe4c2f0df2c9500
MrVulcan <wkr1wkr@163.com>
2022-06-29 12:25:32 +0800

    just trying

--- Client/try.py
+++ Client/try.py
@@ -1,12 +1,23 @@
-import tkinter
-root = tkinter.Tk()
-menu = tkinter.Menu(root,tearoff=0)
-menu.add_command(label="Copy")
-menu.add_command(label="Paste")
-menu.add_separator()
-menu.add_command(label="Cut")
-def popupmenu(event):
-    menu.post(event.x_root,event.y_root)
-root.bind("<Button-2>",popupmenu)
+import tkinter as tk
+from tkinter import filedialog
+
+
+def select_file():
+    # 单个文件选择
+    selected_file_path = filedialog.askopenfilename()  # 使用askopenfilename函数选择单个文件
+    select_path.set(selected_file_path) 
+
+root = tk.Tk()
+root.title("选择文件或文件夹，得到路径")
+
+# 初始化Entry控件的textvariable属性值
+select_path = tk.StringVar()
+
+# 布局控件
+tk.Label(root, text="文件路径：").grid(column=0, row=0, rowspan=3)
+tk.Entry(root, textvariable = select_path).grid(column=1, row=0, rowspan=3)
+tk.Button(root, text="选择单个文件", command=select_file).grid(row=0, column=2)
+
+
 root.mainloop()
 
