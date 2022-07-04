dfcb108bbf89848f1008c1b5395c41fcf047e4c4
zuobiao-zhou <1373529784@qq.com>
2022-07-04 11:24:21 +0800

    .

--- Client/Client_Network.py
+++ Client/Client_Network.py
@@ -18,8 +18,8 @@ File_PORT = 3600
 Sys_PORT = 3700
 send_2_server = ""
 rcv_size = 1024
-default_file_path = Path("./file_received/")
-default_pic_path = Path("./pic_received/")
+default_file_path = Path("../file_received/")
+default_pic_path = Path("../pic_received/")
 group_message_queue = Queue()
 
 
